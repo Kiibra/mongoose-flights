cc98210443401400a502e86cc0f8c67b0eab0971
kiibra <kibra99@gmail.com>
2024-04-16 18:53:06 -0400

    complete creating list and form for tickets

--- models/flight.js
+++ models/flight.js
@@ -3,6 +3,19 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+  const ticketSchema = new Schema({
+    seat: {
+      type: String,
+      match: /[A-F][1-9]\d?/
+    },
+    price: {
+      type: Number,
+      min: 0
+    },
+  }, {
+    timestamps: true
+  })
+
 const flightSchema = new Schema ({
   airline: {
     type: String,
@@ -27,6 +40,8 @@ const flightSchema = new Schema ({
       return ajustedYear
     },
   },
+
+  tickets: [ticketSchema],
 
 }, {
   timestamps: true
